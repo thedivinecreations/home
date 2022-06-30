2f33e4c106a2d1d7cf61c33e1ee6270fd22963f5
Bikramjeet Sarmah <9854bikram@gmail.com>
2022-06-30 14:44:51 +0530

    delete

--- components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Layout } from "./Layout";
-export { default as MainScreen } from "./MainScreen";
-export { default as Contact } from "./Contact";
-export { default as Testimonials } from "./Testimonials";
