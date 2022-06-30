b3118c78495fe31c66141a730ab8dece9fb1aac7
Bikramjeet Sarmah <9854bikram@gmail.com>
2022-06-30 11:54:59 +0530

    Finished

---
+++ components/About.js
@@ -0,0 +1,16 @@
+const About = () => {
+  return (
+    <div className="relative flex flex-col items-center">
+      <h1 className="p-3 my-32 text-5xl border-b-2 border-white">About</h1>
+      <div className="text-3xl text-center ">
+        Divine creations is a premium design company that focuses on quality,
+        innovation & speed. We are India's leading website designing company. We
+        are <span className="text-blue-300"> AFFORDABLE</span> development
+        company which suits your budget. We utilized technology to bring results
+        to <span className="text-blue-300">Grow</span> our clients businesses.
+      </div>
+    </div>
+  );
+};
+
+export default About;
